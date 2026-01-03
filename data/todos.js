67633314bb14630ada7d4f87716d7070156e0432
Tejasvi1437 <tejasviselvan@gmail.com>
2026-01-03 22:06:44 +0530

    Initial commit for study2

---
+++ data/todos.js
@@ -0,0 +1,5 @@
+export const data=[
+  {"id":1,"title":"Buy groceries","completed":false},
+  {"id":2,"title":"Walk the dog","completed":true},
+  {"id":3,"title":"Read a book","completed":false}
+]
